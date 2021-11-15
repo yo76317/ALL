163dcb5f870f4c2772104952022f0a719298fd5e
yo76317 <yo76317@gmail.com>
2021-11-15 13:39:07 +0800

    剛少新增到一個資料表，現在補上，共兩個

--- PHP/Archive/2021-11-15_Include/member.php/memeber.sql
+++ PHP/Archive/2021-11-15_Include/member.php/memeber.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2021-11-15 06:36:44
+-- 產生時間： 2021-11-15 06:38:31
 -- 伺服器版本： 10.4.21-MariaDB
 -- PHP 版本： 7.4.25
 
@@ -20,6 +20,19 @@ SET time_zone = "+00:00";
 --
 -- 資料庫: `memeber`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `account` varchar(32) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `password` varchar(32) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `mail` varchar(32) COLLATE utf8mb4_unicode_520_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 -- --------------------------------------------------------
 
@@ -42,6 +55,12 @@ CREATE TABLE `member` (
 --
 
 --
+-- 資料表索引 `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `member`
 --
 ALTER TABLE `member`
@@ -50,6 +69,12 @@ ALTER TABLE `member`
 --
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `member`
